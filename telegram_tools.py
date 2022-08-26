e8b4ce72fcf6927b3decb65c45a74534dadf58f1
gbvsilva <graco.babeuf18@gmail.com>
2022-08-26 10:42:11 -0300

    Send to pairs of Telegram auth tokens and chat IDs

--- telegram_tools.py
+++ telegram_tools.py
@@ -2,10 +2,12 @@ import requests
 from get_time import getHour
 from os import environ as env
 
+# Send to pairs of Telegram auth tokens and chat IDs
 def sendToBot(message):
-    try:
-        requests.post(f"https://api.telegram.org/bot{env.get('TELEGRAM_BOT_AUTH_TOKEN')}/sendMessage", 
-                        data={'text': message, 'chat_id': env.get('TELEGRAM_BOT_CHAT_ID')})
-    except requests.exceptions.ConnectionError as err:
-        message = f"{getHour()} -> Erro no post para o Telegram. {err}"
-        print(message)
+    for auth_token, chat_id in zip(env.get('TELEGRAM_BOT_AUTH_TOKENS').split(','), env.get('TELEGRAM_BOT_CHAT_IDS').split(',')):
+        try:
+            requests.post(f"https://api.telegram.org/bot{auth_token}/sendMessage", 
+                            data={'text': message, 'chat_id': chat_id})
+        except requests.exceptions.ConnectionError as err:
+            message = f"{getHour()} -> Erro no post para o Telegram. {err}"
+            print(message)
