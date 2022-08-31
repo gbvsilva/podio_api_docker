22e79d769851a55e23db3e41c8e326f35819aa54
gbvsilva <graco.babeuf18@gmail.com>
2022-08-31 10:25:04 -0300

    Fixed timestamp conversion

--- podio_insert_items.py
+++ podio_insert_items.py
@@ -49,21 +49,24 @@ def insertItems(podio, apps_ids):
                         for item in items:
                             # Buscando a última atualização do Item no banco
                             cursor.execute(f"SELECT \"last_event_on\" FROM podio.{tableName} WHERE id='{item['item_id']}'")
-                            last_event_on_podio = time.mktime(datetime.datetime.strptime(item['last_event_on'], 
+                            last_event_on_podio = item['last_event_on']
+                            time1 = time.mktime(datetime.datetime.strptime(last_event_on_podio, 
                                                     "%Y-%m-%d %H:%M:%S").timetuple())
                             if cursor.rowcount > 0:
                                 last_event_on_db = time.mktime(datetime.datetime.strptime(cursor.fetchone()[0], 
                                                     "%Y-%m-%d %H:%M:%S").timetuple())
-                                if last_event_on_podio > last_event_on_db:
+                                time2 = time.mktime(datetime.datetime.strptime(last_event_on_db, 
+                                                    "%Y-%m-%d %H:%M:%S").timetuple())
+                                if time1 > time2:
                                     hour = getHour()
                                     message = f"{hour} -> Item com ID={item['item_id']} atualizado no Podio. Excluindo-o da tabela '{tableName}'"
                                     print(message)
                                     sendToBot(message)
                                     cursor.execute(f"DROP FROM podio.{tableName} WHERE id='{item['item_id']}'")
 
-                            if cursor.rowcount == 0 or last_event_on_podio > last_event_on_db:
+                            if cursor.rowcount == 0 or time1 > time2:
                                 query = [f"INSERT INTO podio.{tableName}", " VALUES", "("]
-                                query.extend([f"'{str(item['item_id'])}','{str(item['created_on'])}',{last_event_on_podio},"])
+                                query.extend([f"'{str(item['item_id'])}','{str(item['created_on'])}','{last_event_on_podio}',"])
                                 fields = [x for x in item['fields'] if f"\"{x['external_id'][:40]}\"" in tableLabels]
                                 # Fazendo a comparação entre os campos existentes e os preenchidos
                                 # Caso o campo esteja em branco no Podio, preencher com '?'
