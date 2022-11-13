8a3466c44122b58b547b969d45eaf42ffe3026d5
gbvsilva <graco.babeuf18@gmail.com>
2022-11-13 12:00:51 -0300

    Removidos espaços em branco

--- telegram_tools.py
+++ telegram_tools.py
@@ -6,7 +6,7 @@ from os import environ as env
 def sendToBot(message):
     for auth_token, chat_id in zip(env.get('TELEGRAM_BOT_AUTH_TOKENS').split(','), env.get('TELEGRAM_BOT_CHAT_IDS').split(',')):
         try:
-            requests.post(f"https://api.telegram.org/bot{auth_token}/sendMessage", 
+            requests.post(f"https://api.telegram.org/bot{auth_token}/sendMessage",
                             data={'text': message, 'chat_id': chat_id})
         except requests.exceptions.ConnectionError as err:
             message = f"{getHour()} -> Erro no post para o Telegram. {err}"
